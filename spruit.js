fd62d01096007df434d4a800a4a7226cea12cfe4
Ryan Williams <ryan.blake.williams@gmail.com>
2015-05-14 00:10:11 +0000

    update mongo fields, add start of /job page

--- spruit.js
+++ spruit.js
@@ -3,6 +3,55 @@ Jobs = new Mongo.Collection("jobs")
 Stages = new Mongo.Collection("stages")
 Tasks = new Mongo.Collection("tasks")
 
+function jobsWithStages(queryObj) {
+  var jobs = Jobs.find(queryObj || {});
+  console.log("pre jobs: %O", jobs);
+  var stagesById = {}
+  var stageIDs = []
+  jobs.forEach(function(job) {
+    console.log("job: %O", job);
+    stageIDs = stageIDs.concat(job.stageIDs);
+  });
+  console.log("fetching stages: %s", stageIDs.join(','));
+  Stages.find({id: { '$in': stageIDs }}).map(function(stage) {
+    if (!(stage.id in stagesById)) {
+      stagesById[stage.id] = []
+    }
+    stagesById[stage.id].push(stage);
+  });
+  console.log("stagesById: %O", stagesById);
+  var j = jobs.map(function(job) {
+    return {
+      id: job.id,
+      startTime: job.startTime,
+      endTime: job.endTime,
+      succeeded: job.succeeded,
+      inProgress: !job.endTime,
+      failed: job.endTime && !job.succeeded,
+      stages: job.stageIDs.map(function (stageId) {
+        var attempts = stagesById[stageId];
+        return attempts ? attempts[attempts.length - 1] : [];
+      })
+    }
+  });
+  return j;
+}
+
+Router.route("/", function() {
+  this.render('jobsPage');
+});
+
+Router.route("/jobs", function() {
+  this.render('jobsPage');
+});
+
+Router.route("/job/:_id", function() {
+  console.log("params: %O", this.params._id);
+  var jobs = jobsWithStages({ id: parseInt(this.params._id) });
+  console.log("jobs: %O", jobs);
+  this.render('jobPage', { data: jobs.length ? jobs[0] : null });
+});
+
 if (Meteor.isClient) {
 
   function formatTime(ms) {
@@ -17,39 +66,20 @@ if (Meteor.isClient) {
     return moment(dt).format("YYYY/MM/DD HH:mm:ss");
   });
 
+  Template.registerHelper("jobStatus", function(job) {
+    if (job.succeeded) return "SUCCEEDED";
+    if (job.failed) return "FAILED";
+    if (job.inProgress) return "RUNNING";
+    return "UNKNOWN";
+  });
+
+  Template.registerHelper("formatDuration", function(start, end) {
+    return end ? formatTime(end - start) : (formatTime(moment().unix()*1000 - start) + '...');
+  });
+
   Template.jobRows.helpers({
     jobs: function() {
-      var jobs = Jobs.find();
-      var stagesById = {}
-      var stageIDs = []
-      jobs.forEach(function(job) {
-        stageIDs = stageIDs.concat(job.stageIDs);
-      });
-      console.log("fetching stages: %s", stageIDs.join(','));
-      Stages.find({id: { '$in': stageIDs }}).map(function(stage) {
-        if (!(stage.id in stagesById)) {
-          stagesById[stage.id] = []
-        }
-        stagesById[stage.id].push(stage);
-      });
-      console.log("stagesById: %O", stagesById);
-      var j = jobs.map(function(job) {
-        return {
-          id: job.id,
-          time: job.time,
-          finishTime: job.finishTime,
-          succeeded: job.succeeded,
-          inProgress: !job.finishTime,
-          failed: job.finishTime && !job.succeeded,
-          stages: job.stageIDs.map(function (stageId) {
-            var attempts = stagesById[stageId];
-            return attempts ? attempts[attempts.length - 1] : [];
-          })
-        }
-      });
-      console.log("jobs: %O", j);
-      window.jobs = j;
-      return j;
+      return jobsWithStages();
     },
 
     rowClass: function(job) {
@@ -65,11 +95,11 @@ if (Meteor.isClient) {
     },
 
     getJobDuration: function(job) {
-      return job.finishTime ? formatTime(job.finishTime - job.time) : (formatTime(moment().unix()*1000 - job.time) + '...');
+      return job.endTime ? formatTime(job.endTime - job.startTime) : (formatTime(moment().unix()*1000 - job.startTime) + '...');
     },
 
     getSucceededStages: function(job) {
-      return job.stages.filter(function(stage) { return stage.completionTime && !stage.failureReason; }).length;
+      return job.stages.filter(function(stage) { return stage.endTime && !stage.failureReason; }).length;
     },
 
     getSucceededTasks: function(job) {
@@ -97,4 +127,26 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.jobPage.helpers({
+    numCompleteStages: function(job) {
+      return job.stages.filter(function(stage) {
+        return !!stage.completionTime;
+      }).length;
+    },
+    numSucceededStages: function(job) {
+      return job.stages.filter(function(stage) {
+        return !!stage.completionTime && !stage.failed;
+      }).length;
+    },
+    numFailedStages: function(job) {
+      return job.stages.filter(function(stage) {
+        return stage.failed;
+      }).length;
+    },
+    numRunningStages: function(job) {
+      return job.stages.filter(function(stage) {
+        return !stage.completionTime;
+      }).length;
+    }
+  });
 }
