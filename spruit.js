be2b921fa18082cb6cfb390d8aef6810ff9b264f
Ryan Williams <ryan.blake.williams@gmail.com>
2015-05-14 22:11:01 +0000

    add better duration/bytes-formatting function

--- spruit.js
+++ spruit.js
@@ -30,6 +30,13 @@ Router.route("/jobs", function() {
   this.render('jobsPage');
 });
 
+sigFigs = function(m, n) {
+  n = n || 3;
+  console.log("getting %d sigfigs for %d", n, m);
+  var leftOfDecimal = Math.ceil(Math.log(m) / Math.log(10));
+  return m.toFixed(Math.max(0, n - leftOfDecimal));
+}
+
 Router.route("/job/:_id", function() {
   console.log("params: %O", this.params._id);
   var job = Jobs.findOne( { id: parseInt(this.params._id) });
@@ -45,7 +52,34 @@ Router.route("/job/:_id", function() {
 if (Meteor.isClient) {
 
   function formatTime(ms) {
-    return ms + "ms";
+    var S = 1000;
+    var M = 60*S;
+    var H = 60*M;
+    var D = 24*H;
+
+    if (ms < M) {
+      if (ms < S) {
+        return ms + 'ms';
+      }
+      return sigFigs(ms/1000) + 's';
+    }
+
+    var highestLevel = -1;
+    var levels = [[D,'d'],[H,'h'],[M,'m'],[S,'s']/*,[1,'ms']*/];
+    var r =
+          levels.map(function(level, idx) {
+            if (ms > level[0]) {
+              if (highestLevel < 0) {
+                highestLevel = idx;
+              }
+              var v = Math.floor(ms / level[0]);
+              ms -= v*level[0];
+              return v+level[1];
+              return v;
+            }
+          });
+
+    return [r[highestLevel], r[highestLevel+1]].join('');
   }
 
   Template.registerHelper("log", function(something) {
@@ -54,6 +88,22 @@ if (Meteor.isClient) {
 
   Template.registerHelper("formatDateTime", function(dt) {
     return moment(dt).format("YYYY/MM/DD HH:mm:ss");
+  });
+
+  Template.registerHelper("formatBytes", function(bytes) {
+    if (bytes == 0) return "-";
+    var base = 1024;
+    var cutoff = 2;
+    var levels = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    for (var i = 0; i < levels.length; i++) {
+      var order = levels[i];
+      if (bytes < cutoff*base || order == 'PB') {
+        var r = sigFigs(bytes) + order;
+        console.log("returning.. %s", r)
+        return r;
+      }
+      bytes /= 1024;
+    }
   });
 
   Template.registerHelper("jobStatus", function(job) {
