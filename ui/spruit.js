ad1e23582443b9478d21ba8d3bb766183cbe16db
Ryan Williams <ryan.blake.williams@gmail.com>
2015-06-10 21:53:50 +0000

    add stage/task attempts collections

--- ui/spruit.js
+++ ui/spruit.js
@@ -2,7 +2,9 @@
 Applications = new Mongo.Collection("apps");
 Jobs = new Mongo.Collection("jobs");
 Stages = new Mongo.Collection("stages");
+StageAttempts = new Mongo.Collection("stage_attempts");
 Tasks = new Mongo.Collection("tasks");
+TaskAttempts = new Mongo.Collection("task_attempts");
 Executors = new Mongo.Collection("executors");
 RDDs = new Mongo.Collection("rdds");
 
