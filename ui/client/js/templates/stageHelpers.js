b342da77b6bb7fffe170e4f9fb9f3320f8856fd1
Ryan Williams <ryan.blake.williams@gmail.com>
2015-06-11 22:45:49 +0000

    fix stage table sorting

--- ui/client/js/templates/stageHelpers.js
+++ ui/client/js/templates/stageHelpers.js
@@ -1,4 +1,10 @@
 
+attachStagesToAttempts = function(attempts) {
+  return attempts.map(function(stageAttempt) {
+    return { attempt: stageAttempt, stage: Stages.findOne({ id: stageAttempt.stageId }) };
+  });
+};
+
 completedStages = function() {
   return StageAttempts.find({
     $or: [
@@ -6,7 +12,7 @@ completedStages = function() {
       { skipped: true },
       { "time.end": { $exists: true }}
     ]
-  }, { sort: { id: -1 }});
+  }, { sort: { stageId: -1 }});
 };
 Template.registerHelper("completedStages", function() { return attachStagesToAttempts(completedStages()); });
 Template.registerHelper("numCompletedStages", function() {
@@ -22,7 +28,7 @@ activeStages = function() {
     ended: { $not: true },
     skipped: { $not: true },
     "time.end": { $exists: false }
-  }, { sort: { id: -1 }});
+  }, { sort: { stageId: -1 }});
 };
 Template.registerHelper("activeStages", function() { return attachStagesToAttempts(activeStages()); });
 Template.registerHelper("numActiveStages", function() {
@@ -36,23 +42,17 @@ pendingStages = function() {
     ended: { $not: true },
     skipped: { $not: true },
     "time.end": { $exists: false }
-  }, { sort: { id: -1 }});
+  }, { sort: { stageId: -1 }});
 };
 Template.registerHelper("pendingStages", function() { return attachStagesToAttempts(pendingStages()); });
 Template.registerHelper("numPendingStages", function() {
   return pendingStages().count();
 });
 
-attachStagesToAttempts = function(attempts) {
-  return attempts.map(function(stageAttempt) {
-    return { attempt: stageAttempt, stage: Stages.findOne({ id: stageAttempt.stageId }) };
-  });
-};
-
 skippedStages = function() {
   return StageAttempts.find({
     skipped: true
-  });
+  }, { sort: { stageId: -1 }});
 };
 Template.registerHelper("skippedStages", function() { return attachStagesToAttempts(skippedStages()); });
 Template.registerHelper("numSkippedStages", function() {
