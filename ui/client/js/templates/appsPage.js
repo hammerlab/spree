27c643cd295104c76bb017b75ca46d137fee7570
Ryan Williams <ryan.blake.williams@gmail.com>
2015-06-22 21:43:01 +0000

    port /apps page to 'sortable-table' widget

--- ui/client/js/templates/appsPage.js
+++ ui/client/js/templates/appsPage.js
@@ -1,7 +1,34 @@
 
+var columns = [
+  { id: 'id', label: 'App ID', cmpFn: sortBy('id') },
+  { id: 'name', label: 'App Name', cmpFn: sortBy('name') },
+  { id: 'start', label: 'Started', cmpFn: sortBy('time.start') },
+  { id: 'end', label: 'Completed', cmpFn: sortBy('time.end') },
+  { id: 'duration', label: 'Duration', cmpFn: sortBy(function(a) { return a.time.end - a.time.start; }) },
+  { id: 'user', label: 'User', cmpFn: sortBy('user') }
+];
+
+var columnsById = {};
+columns.forEach(function(column) {
+  columnsById[column.id] = column;
+  column.template = 'appRow-' + column.id;
+  column.table = 'app-table';
+});
+
+
+
 Template.appsPage.helpers({
   applications: function() {
-    return Applications.find();
-  }
+    var sort = Session.get('app-table-sort') || ['start', -1];
+    var cmpFn = columnsById[sort[0]].cmpFn;
+    var apps = Applications.find().map(function(app) { return app; });
+    if (cmpFn) {
+      return sort[1] == 1 ? apps.sort(cmpFn) : apps.sort(cmpFn).reverse();
+    } else {
+      return sort[1] == 1 ? apps.sort() : apps.sort().reverse();
+    }
+  },
+
+  columns: function() { return columns; }
 });
 
