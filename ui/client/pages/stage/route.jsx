a69c1e2a51c7058f0b5b14c0e3da5ce1d08e4997
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-08 01:34:38 +0000

    cleaner url params in stage page route

--- ui/client/pages/stage/route.jsx
+++ ui/client/pages/stage/route.jsx
@@ -23,34 +23,34 @@ var statRows = [
 // StageAttempt page
 Router.route("/a/:_appId/stage/:_stageId", {
   waitOn: function() {
+    var appId = this.params._appId;
+    var stageId = parseInt(this.params._stageId);
+    var attemptId = this.params.query.attempt ? parseInt(this.params.query.attempt) : 0;
     return [
-      Meteor.subscribe(
-            'stage-page',
-            this.params._appId,
-            parseInt(this.params._stageId),
-            this.params.query.attempt ? parseInt(this.params.query.attempt) : 0
-      )
+      Meteor.subscribe('stage-page', appId, stageId, attemptId, Cookie.get("tasks-table-opts"))
     ];
   },
   action: function() {
+    var appId = this.params._appId;
+    var stageId = parseInt(this.params._stageId);
+    var attemptId = this.params.query.attempt ? parseInt(this.params.query.attempt) : 0;
     var stageAttempt = StageAttempts.findOne();
+
     if (!stageAttempt) {
       this.render('stagePage', {
         data: {
-          appId: this.params._appId,
+          appId: appId,
           app: Applications.findOne(),
-          stageId: parseInt(this.params._stageId),
-          attemptId: this.params.query.attempt ? parseInt(this.params.query.attempt) : 0,
           executors: Executors.find(),
           stats: [],
           taskAttempts: TaskAttempts.find(),
+          stageId: stageId,
+          attemptId: attemptId,
           stagesTab: 1
         }
       });
       return;
     }
-    var stageId = stageAttempt.stageId;
-    var attemptId = stageAttempt.id;
 
     var stats = [];
     statRows.forEach(function(c) {
@@ -76,7 +76,7 @@ Router.route("/a/:_appId/stage/:_stageId", {
 
     this.render('stagePage', {
       data: {
-        appId: this.params._appId,
+        appId: appId,
         app: Applications.findOne(),
         stageAttempt: stageAttempt,
         stageId: stageId,
