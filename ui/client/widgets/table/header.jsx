ab6dcbc34c15ab4426cc3a4a8c80e4db939c427d
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-11 23:23:25 +0000

    fix sorting with multi-key columns
    fixes #23

--- ui/client/widgets/table/header.jsx
+++ ui/client/widgets/table/header.jsx
@@ -1,42 +1,29 @@
 
 function getKey(o) {
-  for (k in o) return k;
+  for (var k in o) return k;
 }
 
 TableHeader = React.createClass({
   onClick(e) {
-    if (this.props.clientSort) {
-      var extantSort = Cookie.get(this.props.tableSortKey);
-      var newSort =
-            (extantSort && extantSort.id == this.props.id) ?
-            {id: this.props.id, dir: -extantSort.dir} :
-            {id: this.props.id, dir: this.props.defaultSort || 1};
+    var extantSort = Cookie.get(this.props.tableSortKey);
+    var newSort =
+          (extantSort && extantSort.id == this.props.id) ?
+          {id: this.props.id, dir: -extantSort.dir} :
+          {id: this.props.id, dir: this.props.defaultSort || 1};
 
-      Cookie.set(this.props.tableSortKey, newSort);
-    } else {
+    Cookie.set(this.props.tableSortKey, newSort);
 
-      var extantOpts = Cookie.get(this.props.tableOptsKey) || {};
-      var extantSort = extantOpts.sort;
-
-      var newSort = {};
-      newSort[this.props.sortKey] =
-            (extantSort && (this.props.sortKey in extantSort)) ?
-                  -extantSort[this.props.sortKey] :
-                  (this.props.defaultSort || 1);
-
-      var newOpts = jQuery.extend(extantOpts, {sort: newSort});
-      Cookie.set(this.props.tableOptsKey, newOpts);
-    }
+    var extantOpts = Cookie.get(this.props.tableOptsKey) || {};
+    var newSortOpt = {};
+    this.props.sortKeys.forEach((k) => {
+      newSortOpt[k] = newSort.dir;
+    });
+    var newOpts = jQuery.extend(extantOpts, { sort: newSortOpt });
+    Cookie.set(this.props.tableOptsKey, newOpts);
   },
   isCurrentSort() {
-    if (this.props.clientSort) {
-      var extantSort = Cookie.get(this.props.tableSortKey);
-      return extantSort && extantSort.id == this.props.id ? extantSort.dir : 0;
-    } else {
-      var opts = Cookie.get(this.props.tableOptsKey) || {};
-      var sort = opts.sort;
-      return sort && getKey(sort) == this.props.sortKey ? sort[this.props.sortKey] : 0;
-    }
+    var extantSort = Cookie.get(this.props.tableSortKey);
+    return extantSort && extantSort.id == this.props.id ? extantSort.dir : 0;
   },
   render() {
     var opts = Cookie.get(this.props.tableOptsKey) || {};
