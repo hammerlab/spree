3375eef96c873f96d90aa64d6380749d3761de00
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-11 23:22:28 +0000

    some fn helpers

--- ui/client/lib/lib/utils/fn-utils.js
+++ ui/client/lib/lib/utils/fn-utils.js
@@ -16,21 +16,23 @@ acc = function(key) {
   }, function(x) { return x; });
 };
 
-//makeCmpFn = function(key) {
-//  var fn = null;
-//  if (typeof key == 'function') {
-//    fn = key;
-//  } else if (typeof key == 'string') {
-//    fn = acc(key);
-//  } else {
-//    throw new Error("Can't sort by: " + key);
-//  }
-//  return function(a, b) {
-//    var fna = fn(a);
-//    var fnb = fn(b);
-//    if (fna < fnb || fna === undefined) return -1;
-//    if (fna > fnb || fnb === undefined) return 1;
-//    return 0;
-//  }
-//};
+function getKeys(o) {
+  var keys = [];
+  for (var k in o) keys.push(k);
+  return keys;
+}
+
+function arrEq(a1, a2) {
+  if (a1.length != a2.length) return false;
+  for (var i = 0; i < a1.length; i++) {
+    if (a1[i] != a2[i]) return false;
+  }
+  return true;
+}
+
+function sameKeys(o1, o2) {
+  if (!o1 && !o2) return true;
+  if (!o1 || !o2) return false;
+  return arrEq(getKeys(o1), getKeys(o2));
+}
 
