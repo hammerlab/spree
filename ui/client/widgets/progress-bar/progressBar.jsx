f61c3cc92305d6891eb835cec8b42128531534ce
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-13 21:51:09 +0000

    progressbar expand if num active + running > total

--- ui/client/widgets/progress-bar/progressBar.jsx
+++ ui/client/widgets/progress-bar/progressBar.jsx
@@ -2,16 +2,22 @@
 ProgressBar = React.createClass({
   render() {
     var counts = this.props.counts;
+    if (!counts) {
+      return null;
+    }
+    var running = counts.running || 0;
+    var succeeded = counts.succeeded || 0;
+    var total = Math.max(running + succeeded, counts.num);
     var label =
-          (counts && counts.succeeded || 0) +
+          (counts.succeeded || 0) +
           "/" +
-          (counts && counts.num || "?") +
-          (counts && counts.running ? (" (" + counts.running + " running)") : "");
+          (total || "?") +
+          (counts.running ? (" (" + counts.running + " running)") : "");
 
     return <div className="progress">
       <span className="progress-label">{label}</span>
-      <div className="bar bar-completed" style={{width: ((counts && (counts.succeeded / counts.num) || "") * 100) + '%'}}></div>
-      <div className="bar bar-running" style={{width: ((counts && (counts.running / counts.num) || "") * 100) + '%'}}></div>
+      <div className="bar bar-completed" style={{width: (((succeeded / total) || 0) * 100) + '%'}}></div>
+      <div className="bar bar-running" style={{width: (((running / total) || 0) * 100) + '%'}}></div>
     </div>;
   }
 });
