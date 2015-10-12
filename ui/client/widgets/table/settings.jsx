9c378f4d7d0a173fc202937570c20ac69488d07f
Ryan Williams <ryan.blake.williams@gmail.com>
2015-10-12 00:28:42 +0000

    put table settings in a position:absolute div

--- ui/client/widgets/table/settings.jsx
+++ ui/client/widgets/table/settings.jsx
@@ -10,32 +10,34 @@ TableSettings = React.createClass({
           className="settings-container"
           onMouseEnter={(e) => { this.props.showSettingsFn(true); }}
           onMouseLeave={(e) => { this.props.showSettingsFn(false); }}>
-      {
-        this.props.showSettingsGear ?
+      <div className="settings-container-abs">
+        {
+              this.props.showSettingsGear ?
               <img
                     className='gear'
                     src='/img/gear.png'
-                    width="20"
+                    width="21"
                     height="20"
-                    /> :
-              null
-      }
-      <div className="settings-tooltip-container">
-        <div className={'settings-tooltip' + (this.props.visible ? '' : ' hidden')}>
-          {
-            this.props.settings.map((setting) => {
-              var id = setting.id;
-              return <TableSettingsTooltipRow
-                    key={id}
-                    id={id}
-                    label={setting.label}
-                    displayed={(id in this.props.displayedMap)}
-                    nonEmpty={(id in this.props.nonEmptyMap)}
-                    canDisplay={(id in this.props.canDisplayMap)}
-                    tableColumnsKey={this.props.tableColumnsKey}
-                    />;
-            })
-          }
+              />
+                    : null
+              }
+        <div className="settings-tooltip-container">
+          <div className={'settings-tooltip' + (this.props.visible ? '' : ' hidden')}>
+            {
+                  this.props.settings.map((setting) => {
+                        var id = setting.id;
+                        return <TableSettingsTooltipRow
+                              key={id}
+                              id={id}
+                              label={setting.label}
+                              displayed={(id in this.props.displayedMap)}
+                              nonEmpty={(id in this.props.nonEmptyMap)}
+                              canDisplay={(id in this.props.canDisplayMap)}
+                              tableColumnsKey={this.props.tableColumnsKey}
+                        />;
+                        })
+                  }
+          </div>
         </div>
       </div>
     </div>
