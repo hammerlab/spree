f3a6a0e30bae300f0b132da10407b0c7e3b6ce2a
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2016-08-01 21:10:52 +1200

    update dag link

--- ui/client/widgets/dag/dag.jsx
+++ ui/client/widgets/dag/dag.jsx
@@ -1,4 +1,10 @@
-SVGGraphElement = React.createClass({
+SVGGraph = React.createClass({
+  render() {
+    return <svg>{"GRAPH"}</svg>;
+  }
+});
+
+GraphRootElement = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
     const subscription = Meteor.subscribe('stage-page-graph', this.props.appId, this.props.stageId);
@@ -9,13 +15,19 @@ SVGGraphElement = React.createClass({
   },
   render() {
     if (this.data.ready) {
-      console.log(this.data.graph);
-      return <pre>{"Found, data is printed in console"}</pre>;
+      if (this.data.graph) {
+        console.log(this.data.graph);
+        return <div><SVGGraph /></div>;
+      } else {
+        var msg = "No visualization information available for this " + this.props.appId +
+          ", stage " + this.props.stageId + ", its visualization data may have been cleaned up.";
+        return <div><b>{msg}</b></div>;
+      }
     } else {
       return <span></span>;
     }
   }
-})
+});
 
 DAGVisualization = React.createClass({
   getInitialState() {
@@ -30,13 +42,13 @@ DAGVisualization = React.createClass({
     var appId = this.props.stageAttempt.appId;
     var stageId = this.props.stageAttempt.stageId;
     console.log("DAG for ", appId, stageId);
-    var link = <a href="#" onClick={this.handleClick}>
-      <span>{(this.state.toggle) ? "▾" : "▸"} </span>
+    var link = <span className="link-collapsed" onClick={this.handleClick}>
+      <span className="link-toggle">{(this.state.toggle) ? "▾" : "▸"} </span>
       <span>{this.props.name}</span>
-    </a>;
+    </span>;
 
     if (this.state.toggle) {
-      return <div>{link}<SVGGraphElement appId={appId} stageId={stageId} /></div>;
+      return <div>{link}<GraphRootElement appId={appId} stageId={stageId} /></div>;
     } else {
       return <div>{link}</div>;
     }
